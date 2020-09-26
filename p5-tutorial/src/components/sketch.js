eae31378080b183f993fa05bcfea4bc808b84fa2
giorgiomartini <jorgeantonio82@googlemail.com>
2020-09-27 01:49:51 +0200

    added uniform grid

--- p5-tutorial/src/components/sketch.js
+++ p5-tutorial/src/components/sketch.js
@@ -1,26 +1,3 @@
-// export default function Sketch(p5) {
-//     let canvas;
-
-//     p5.setup = () => {
-//       canvas = p5.createCanvas(400, 400);
-//       p5.noStroke();
-//     };
-
-//     p5.draw = () => {
-//       let y = p5.color("coral");
-
-//       p5.fill(y);
-//       p5.circle(45, 45, 65);
-
-//       if (p5.mouseIsPressed) {
-//         p5.fill(p5.color("lightsalmon"));
-//       } else {
-//         p5.fill(p5.color("hsl(160, 100%, 50%)"));
-//       }
-//       p5.circle(p5.mouseX, p5.mouseY, 70, 70);
-//     };
-//   }
-
 const between = (x, min, max) =>  x >= min && x <= max
 
 function isOverGridPoint (mouseX, mouseY, x, y, range = 20) {
@@ -32,19 +9,17 @@ function isOverGridPoint (mouseX, mouseY, x, y, range = 20) {
 export default function Sketch(p5) {
   let canvas
   let colorPicker
-  let cols = 40
-  let rows = 20
+  let cols = 20
+  let rows
   let spots = []
   const windowResized = (p5) => p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
-  const lineThresh = 30
-  // let color = "#464C7C"
+  const lineThresh = 40
   let color = "#2b1b69"
 
   function Spot(p5, x, y) {
     let _x = x
     let _y = y
     let _r = 2
-    let opacity = 255
     let hovered = false
 
     function display() {
@@ -92,14 +67,15 @@ export default function Sketch(p5) {
   }
 
   p5.setup = (canvasParentRef) => {
-    // p5.pixelDensity(1);
     canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight /*, p5.WEBGL*/)
     colorPicker = p5.createColorPicker(color);
     colorPicker.position(0, p5.height + 5);
-    const colWidth = (p5.width / cols)
-    const colHeight = (p5.height / rows)
-    canvas.position(0, 0)
-    canvas.style('z-index', '-1')
+    const colWidth  = p5.width/cols
+    const colHeight = colWidth
+    rows = p5.height/colWidth
+
+    canvas.position(0, 0).style('z-index', '-1')
+
     p5.background(0);
     for (let x = 0; x < cols+1; x++) {
       for (let y = 0; y < rows+1; y++) {
@@ -111,7 +87,6 @@ export default function Sketch(p5) {
   p5.draw = () => {
     p5.background(0)
     color = colorPicker.color()
-    // p5.translate(-p5.width / 2, -p5.height / 2,0);
     p5.strokeWeight(1)
     spots.forEach((s, i) => {
       let { _x, _y, hovered } = s.values()
@@ -130,5 +105,4 @@ export default function Sketch(p5) {
 
     })
   };
-  // return <Sketch windowResized={windowResized} setup={setup} draw={draw} />
 }
