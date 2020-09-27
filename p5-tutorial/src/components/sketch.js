1faabcb224b6555bfc02fe1c5aac254a0e90f1e7
giorgiomartini <jorgeantonio82@googlemail.com>
2020-09-27 20:58:58 +0200

    added color and responsive grid

--- p5-tutorial/src/components/sketch.js
+++ p5-tutorial/src/components/sketch.js
@@ -6,6 +6,7 @@ function isOverGridPoint (mouseX, mouseY, x, y, range = 20) {
     : false
 }
 
+// https://coolors.co/ff6978-fffcf9-b1ede8-6d435a-352d39
 export default function Sketch(p5) {
   let canvas
   let colorPicker
@@ -14,7 +15,9 @@ export default function Sketch(p5) {
   let spots = []
   const windowResized = (p5) => p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
   const lineThresh = 40
-  let color = "#2b1b69"
+  // let color = "#2b1b69"
+  // let color = "#3E85BB"
+  let color = '#FF6978'
 
   function Spot(p5, x, y) {
     let _x = x
@@ -44,9 +47,6 @@ export default function Sketch(p5) {
     function randomWalk() {
       _x += p5.random(-0.5,0.5)
       _y += p5.random(-0.5,0.5)
-      // if (_y >= p5.windowHeight) {
-      //   _y = 0
-      // }
     }
 
     function values () {
@@ -66,27 +66,26 @@ export default function Sketch(p5) {
     }
   }
 
-  p5.setup = (canvasParentRef) => {
+  p5.setup = () => {
     canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight /*, p5.WEBGL*/)
-    colorPicker = p5.createColorPicker(color);
-    colorPicker.position(0, p5.height + 5);
-    const colWidth  = p5.width/cols
-    const colHeight = colWidth
-    rows = p5.height/colWidth
+    // colorPicker = p5.createColorPicker(color);
+    // colorPicker.position(0, p5.height + 5);
+    cols = p5.map(p5.width, 0, 2000 , 2, 30)
+    const gridSize  = p5.width/cols
+    rows = p5.height/gridSize
 
     canvas.position(0, 0).style('z-index', '-1')
-
-    p5.background(0);
+    p5.background('#352D39');
     for (let x = 0; x < cols+1; x++) {
       for (let y = 0; y < rows+1; y++) {
-        spots.push(Spot(p5, x * colWidth, y * colHeight))
+        spots.push(Spot(p5, x * gridSize, y * gridSize))
       }
     }
   }
 
   p5.draw = () => {
-    p5.background(0)
-    color = colorPicker.color()
+    p5.background('#352D39')
+    // color = colorPicker.color()
     p5.strokeWeight(1)
     spots.forEach((s, i) => {
       let { _x, _y, hovered } = s.values()
@@ -98,11 +97,15 @@ export default function Sketch(p5) {
       spots.forEach(ref => {
         if (between(_x, ref.values()._x-lineThresh, ref.values()._x+lineThresh) && between(_y, ref.values()._y-lineThresh, ref.values()._y+lineThresh)) {
           p5.stroke(color)
-          p5.strokeWeight(3)
+          p5.strokeWeight(1)
           p5.line(_x, _y, ref.values()._x, ref.values()._y)
         }
       })
 
     })
-  };
+  }
+
+  p5.mouseClicked = () => {
+    console.log(8)
+  }
 }
--- p5-tutorial/src/pages/index.js
+++ p5-tutorial/src/pages/index.js
@@ -11,9 +11,9 @@ const IndexPage = () => (
           <p className="mt-16 mb-4 text-6xl">Hey!</p>
           <p className="text-4xl">I'm a Berlin based web developer with a passion for music.</p>
           <p className="text-4xl">I love creating stuff with
-            <Link className="text-gray-600" to='/code'> code</Link>, making
-            <Link className="text-gray-600" to='/code'> music</Link> and taking
-            <Link className="text-gray-600" to='/code'> pics</Link>.
+            <Link className="gioGreen" to='/code'> code</Link>, making
+            <Link className="gioGreen" to='/code'> music</Link> and taking
+            <Link className="gioGreen" to='/code'> pics</Link>.
           </p>
           <p className="text-4xl">You can find me on twitter, soundcloud and instagram.</p>
         </div>
