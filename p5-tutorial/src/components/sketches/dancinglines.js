2adb4daa6ebeb41331578fb0d27a087a8d5fb926
giorgiomartini <jorgeantonio82@googlemail.com>
2020-10-26 00:50:03 +0100

    normal

--- p5-tutorial/src/components/sketches/dancinglines.js
+++ p5-tutorial/src/components/sketches/dancinglines.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react"
 import {loadableP5 as P5Wrapper} from '../loadable';
+import {Vector} from "p5"
 
 let bgColor = "#18020C"
 let dancingLine1
 let dancingLine2
 let snake
+// let twoCircles
+// let follower
+// let snakeAttractor
 
 function isOdd(num) { return num % 2;}
 const every_nth = (arr, nth) => arr.filter((e, i) => i % nth === nth - 1)
@@ -17,15 +21,15 @@ function isEven(value) {
 }
 
 function dancinglines (p5) {
-
-
   p5.setup = () => {
     p5.createCanvas(700,700)
     p5.background(bgColor)
+    // p5.frameRate(2)
     dancingLine1 = makeDancingLine()
     dancingLine2 = makeDancingLine(true)
-    snake = makeSnake()
-    // p5.frameRate(2)
+    snake = Snake()
+    // twoCircles = TwoCircles()
+    // snakeAttractor = SnakeAttractor(Follower)
   }
   
   p5.draw = () => {
@@ -34,8 +38,9 @@ function dancinglines (p5) {
     dancingLine1.display()
     dancingLine2.display()
     snake.display()
+    // twoCircles.display()
+    // snakeAttractor.display()
   }
-
 
   function makeDancingLine(flip) {
     let history = []
@@ -86,79 +91,163 @@ function dancinglines (p5) {
     }
   }  
 
-  function makeSnake() {
+  function Snake() {
     let history = []
     let linesAmt = 100
     let col = 255
     let v1 = p5.createVector(0,0)
     let finalForm = false
     let lowResLine = []
-    
     let x1Angle = 90
     let y1Angle = 90
-  
     let x1AngleOff = 0.01
     let y1AngleOff = 0.002
-    
     let r1 = 250
 
     function display() {
       v1.set(r1 * p5.sin(x1Angle), r1 * p5.sin(y1Angle))
 
-      // let lowResLine = 
       history.push([v1.x, v1.y]) 
+      lowResLine = [history[0], ...every_nth(history, 10), history[history.length-1]]
 
       if (history.length > linesAmt) {
         history.splice(0,1)
         finalForm = true
-        // debugger
       }
-
-      // if final form is true
-
-      lowResLine = [...every_nth(history, 8), history[history.length-1]]
-      // take 1st and last items
-      // and take every nth in the middle
       
       p5.stroke(col) 
       p5.noFill()
       p5.strokeWeight(10)
       p5.strokeCap(p5.PROJECT)
       p5.beginShape()
-
       if (finalForm) {
-        lowResLine.forEach(x => {
+        lowResLine.forEach((x, i) => {
+          // i === lowResLine.length-1 && p5.ellipse(x[0], x[1], 30)
+          // i === 0 && p5.ellipse(x[0], x[1], 30)
           p5.curveVertex(x[0], x[1])
         })      
         console.log('low')
       } else {
-        history.forEach(x => {
+        history.forEach((x,i) => {
+          // i === history.length-1 && p5.ellipse(x[0], x[1], 30)
+          // i === 0 && p5.ellipse(x[0], x[1], 30)
           p5.curveVertex(x[0], x[1])
         })      
         console.log('high')
       }
-      
       p5.endShape()
-
-      // console.log('lowResLine: ', lowResLine.length)
-      // console.log('history: ', history.length)
   
       x1Angle += x1AngleOff  + 0.002
       y1Angle += y1AngleOff  + 0.012
     }
-  
+    return {
+      display,
+    }
+  }
+  function TwoCircles() {
+    let history = []
+    let linesAmt = 100
+    let col = 255
+    let v1 = p5.createVector(0,0)
+    let finalForm = false
+    let lowResLine = []
+    let x1Angle = 0
+    let y1Angle = 30
+    let x1AngleOff = 0.002
+    let y1AngleOff = 0.001
+    let r1 = 300
+
+    function display() {
+      v1.set(r1 * p5.sin(x1Angle), r1 * p5.sin(y1Angle))
+
+      history.push([v1.x, v1.y]) 
+      lowResLine = [history[0], ...every_nth(history, 50), history[history.length-1]]
+
+      if (history.length > linesAmt) {
+        history.splice(0,1)
+        finalForm = true
+      }
+      
+      p5.stroke(col) 
+      p5.noFill()
+      p5.strokeCap(p5.ROUND)
+      if (finalForm) {
+        lowResLine.forEach((x, i) => {
+          p5.strokeWeight(10)
+          i === lowResLine.length-1 && p5.ellipse(x[0], x[1], 30)
+          // p5.strokeWeight(1)
+          // i === 0 && p5.ellipse(x[0], x[1], 10)
+        })      
+      } else {
+        history.forEach((x,i) => {
+          p5.strokeWeight(10)
+          i === history.length-1 && p5.ellipse(x[0], x[1], 30)
+          // p5.strokeWeight(1)
+          // i === 0 && p5.ellipse(x[0], x[1], 10)
+        })      
+      }
+
+      // p5.stroke(255)
+      // p5.strokeWeight(2)
+      // p5.line(lowResLine[0][0], lowResLine[0][1], lowResLine[lowResLine.length-1][0], lowResLine[lowResLine.length-1][1])
+      
+      x1Angle += x1AngleOff  + 0.009
+      y1Angle += y1AngleOff  + 0.008
+    }
     return {
       display,
     }
   }
 
+  // function Follower() {
+  //   let pos = p5.createVector(200,200)
+  //   let vel = Vector.random2D()
+  //   vel.mult(1)
+  //   function display(x,y) {
+  //     let attractor = p5.createVector(x,y)
+  //     let acc = Vector.sub(attractor, pos)
+  //     acc.setMag(0.1)
+  //     vel.add(acc)
+  //     pos.add(vel)
+  //     p5.stroke(255)
+  //     p5.strokeWeight(2)
+  //     p5.ellipse(pos.x, pos.y, 40)
+  //   }
+  //   return {
+  //     display,
+  //   }
+  //  }
 
+  // function SnakeAttractor(Follower) {
+  //   let col = '255,0,0'
+  //   let v1 = p5.createVector(0,0)
+  //   let x1Angle = 90
+  //   let y1Angle = 90
+  //   let x1AngleOff = 0.01
+  //   let y1AngleOff = 0.002
+  //   let r1 = 250
+  //   let followr = Follower()
+
+  //   function display() {
+  //     v1.set(r1 * p5.sin(x1Angle), r1 * p5.sin(y1Angle))
+
+      
+  //     followr.display(v1.x, v1.y)
+  //     p5.stroke(255,0) 
+  //     // p5.strokeWeight(30)
+  //     p5.ellipse(v1.x, v1.y, 10)
+      
+  //     x1Angle += x1AngleOff  + 0.002
+  //     y1Angle += y1AngleOff  + 0.012
+  //   }
+  //   return {
+  //     display,
+  //   }
+  // }
 }
 
 export default class App extends Component{
-
   render() {
     return <P5Wrapper sketch={dancinglines} />
   }
 }
-
